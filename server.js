def6f416f7cc1edf07255d541f30922d7b21c91e
Bettina Staartjes <bbstaars@gmail.com>
2023-02-09 11:30:46 -0500

    testing changes to heroku

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const helpers = require('./utils/helpers');
+
+const app = express();
 const PORT = process.env.PORT || 3001;
+
+
+
+
+
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
