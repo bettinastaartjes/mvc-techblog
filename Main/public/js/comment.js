4de68243b6fc90187c29703cba9bb50d098089bf
Bettina Staartjes <bbstaars@gmail.com>
2023-02-13 18:13:55 -0500

    installed dependencies and made changes to js files in public folder

---
+++ Main/public/js/comment.js
@@ -0,0 +1,7 @@
+async function commentFormHandler (event) {
+    event.preventDefault();
+
+    const post_Id = window.location.toString().split('/')[
+        window.location.toString().split('/').length -1
+    ];
+    }
